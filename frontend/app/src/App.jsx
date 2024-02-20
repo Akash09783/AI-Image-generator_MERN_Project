1ee5a755ed52a22e8b6675afeb747e0580850ba3
Akash <kumawat.aakash515@gmail.com>
2024-02-20 15:04:47 +0530

    home and create page ui design done

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -11,10 +11,10 @@ function App() {
 <BrowserRouter>
 <header className="w-full flex justify-between
 items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] " >
- <Link>
+ <Link to='/' >
  <img src={logo} alt="logo" className='w-28 object-contain ' />
  </Link> 
-<Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md '>
+<Link to='create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md '>
 Create
 </Link>
 </header>
@@ -22,7 +22,7 @@ Create
  w-full bg-[#f9fafe] min-h[calc(100vh-73px)]   '>
 <Routes>
   <Route path='/' element={<Home/>}/>
-  <Route path='/' element={<CreatePost/>}/>
+  <Route path='/create-post' element={<CreatePost/>}/>
 </Routes>
  </main>
    
--- frontend/app/src/pages/CreatePost.jsx
+++ frontend/app/src/pages/CreatePost.jsx
@@ -1,10 +1,69 @@
-
-
+import React from "react"
+import { FormField } from "../components"
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 const CreatePost = () => {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const handleSubmit=()=>{
+
+  }
+  const handleChange=()=>{
+
+  }
+  const handleSurpriseMe=()=>{
+
+  }
+
+
+
+
+
   return (
-    <div>
-      
-    </div>
+  <section className="max-w 7x1 mx-auto">
+     <div className="text-">
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+      Create
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+        Create imaginative and Visually stunning images through AI Image GEN and share with the community
+        </p>
+      </div>
+
+<form className="mt-16 max-w-3x1 " onSubmit={handleSubmit} >
+<div className="flex flex-col gap-5">
+<FormField 
+  LabelName ="text"
+  type = "text"
+  name='name'
+  placeholder = "Akash"
+  value={form.name}
+  handleChange={handleChange}
+
+/>
+<FormField 
+  LabelName ="text"
+  type = "text"
+  name='name'
+  placeholder = "A plush toy robot sitting against a yellow wall"
+  value={form.name}
+  handleChange={handleChange}
+isSurpriseMe
+handleSurpriseMe={handleSurpriseMe}
+/>
+
+</div>
+
+
+
+</form>
+
+  </section>
   )
 }
 
